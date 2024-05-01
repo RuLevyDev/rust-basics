8850284724caa79be64524d77b5fb44178af35ad
Ruben <ruben@thebubblehub.com>
2024-05-01 14:37:12 +0200

    car example hashmapas

---
+++ rust-car-example/target/debug/deps/rust_car_example-a8c5968829a17308.d
@@ -0,0 +1,5 @@
+/Users/rubenlevy/rust-basics/rust-car-example/target/debug/deps/librust_car_example-a8c5968829a17308.rmeta: src/main.rs
+
+/Users/rubenlevy/rust-basics/rust-car-example/target/debug/deps/rust_car_example-a8c5968829a17308.d: src/main.rs
+
+src/main.rs:
---
+++ rust-car-example/target/debug/rust-car-example.d
@@ -0,0 +1,1 @@
+/Users/rubenlevy/rust-basics/rust-car-example/target/debug/rust-car-example: /Users/rubenlevy/rust-basics/rust-car-example/src/main.rs
